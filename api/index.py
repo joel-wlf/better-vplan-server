ba6828ae7b5cbf8062477f6aa3a2cb1c527d13fc
Joel <72318130+joel-wlf@users.noreply.github.com>
2023-12-16 15:02:42 +0100

    fix?

--- api/index.py
+++ api/index.py
@@ -182,7 +182,7 @@ class DSBApi:
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:5173", "https://better-vplan.vercel.app"])
 
 @app.route('/', methods=['POST'])
 def home():
